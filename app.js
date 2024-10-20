a2016becc19e567b26fc3ce173bc70adbc2dae75
minzzz995 <minzzz995@gmail.com>
2024-10-20 22:26:03 +0900

    password change

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const indexRouter = require("./routes/index")
-require('dotenv').config()
-const app = express()
-const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+require('dotenv').config();
+const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 
-app.use(bodyParser.json())
-// app.use(cors())
+app.use(bodyParser.json());
+
+// CORS 설정
 app.use(cors({
     origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
-  }));
-  
-app.use("/api", indexRouter);
+}));
 
-const mongoURI = MONGODB_URI_PROD;
+// API 라우터 설정
+app.use('/api', indexRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {   
-  console.log(`Server is running on port ${PORT}`);
-});
+// 포트 설정
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(mongoURI,{useNewUrlParser:true})
-        .then(() => {
-            console.log("mongoose connected");
-        })
-        .catch((err)=>{
-            console.log("DB connectino fail", err)
+// MongoDB URI가 올바르게 로드되었는지 확인 (여기 추가)
+console.log("MongoDB URI:", MONGODB_URI_PROD);
+
+// MongoDB 연결 및 서버 시작
+mongoose.connect(MONGODB_URI_PROD)
+    .then(() => {
+        console.log('mongoose connected');
+        // 서버 시작
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
         });
-
-app.listen(5000, () => {
-    console.log("server on 5000")
-})
+    })
+    .catch((err) => {
+        console.log('DB connection fail', err);
+    });
